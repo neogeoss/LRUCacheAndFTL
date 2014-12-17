c3fd769e66202b1f1341f1e70f5c0b21a31ed5bb
In Chan Hwang <inchanhwang@Ins-MacBook-Pro.local>
2014-12-17 12:27:22 +0900

    Initial Commit

---
+++ FTLByChan/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FTLByChan
+//
+//  Created by In Chan Hwang on 12/17/14.
+//  Copyright (c) 2014 In Chan Hwang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
